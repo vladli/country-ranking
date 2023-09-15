780bd5e4337c26200c292b7c492bff4e83052a88
Vladislav Li <vladislav.li911@gmail.com>
2023-09-15 13:02:22 +0900

    1

--- src/components/Cards/CardBody.tsx
+++
@@ -1,8 +0,0 @@
-type Props = {
-  children: React.ReactNode;
-};
-const CardBody = ({ children }: Props) => {
-  return <div className="card-body">{children}</div>;
-};
-
-export default CardBody;
--- src/components/Collapse/index.ts
+++
@@ -1,8 +0,0 @@
-import Collapse from "./Collapse";
-import CollapseContent from "./CollapseContent";
-import CollapseTitle from "./CollapseTitle";
-
-export default Object.assign(Collapse, {
-  Title: CollapseTitle,
-  Content: CollapseContent,
-});
